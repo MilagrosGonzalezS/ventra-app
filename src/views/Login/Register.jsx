e4d6409ada9339335058944f8df89335616947aa
Milagros Gonzalez <101905632+MilagrosGonzalezS@users.noreply.github.com>
2023-11-03 15:55:12 -0300

    2/11

--- src/views/Login/Register.jsx
+++ src/views/Login/Register.jsx
@@ -1,7 +1,109 @@
 // import React from 'react'
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 function Register() {
-  return <div>Register</div>;
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data, event) => {
+    event.preventDefault();
+
+    try {
+      console.log(data); // Log the form data
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="border rounded-2xl bg-opacity p-8 w-1/3 mt-48 mx-auto">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="username" className="text-xs xl:text-base">
+          Nombre de usuario
+        </label>
+        <br></br>
+        <input
+          className="bg-transparent p-2 border-lightblue border-2 rounded-xl mt-4 w-full mb-6"
+          {...register("username", { required: true })}
+          id="username"
+          name="username"
+          type="username"
+        />
+        {errors.email && (
+          <span className="text-sm huge:text-base text-red-800 block text-right">
+            Campo obligatorio
+          </span>
+        )}
+        <br></br>
+        <label htmlFor="email" className="text-xs xl:text-base">
+          Email
+        </label>
+        <br></br>
+        <input
+          className="bg-transparent p-2 border-lightblue border-2 rounded-xl mt-4 w-full mb-6"
+          {...register("email", { required: true })}
+          id="email"
+          name="email"
+          type="email"
+        />
+        {errors.email && (
+          <span className="text-sm huge:text-base text-red-800 block text-right">
+            Campo obligatorio
+          </span>
+        )}
+        <br></br>
+        <label htmlFor="password" className="text-xs xl:text-base">
+          Contraseña
+        </label>
+        <br></br>
+        <input
+          className="bg-transparent p-2 border-lightblue border-2 rounded-xl mt-4 w-full mb-6"
+          {...register("password", { required: true })}
+          id="password"
+          name="password"
+          type="password"
+        />
+        {errors.message && (
+          <span className="text-sm huge:text-base text-red-800 block text-right">
+            Campo obligatorio
+          </span>
+        )}
+        <br></br>
+        <label htmlFor="password2" className="text-xs xl:text-base">
+          Repetir contraseña
+        </label>
+        <br></br>
+        <input
+          className="bg-transparent p-2 border-lightblue border-2 rounded-xl mt-4 w-full mb-6"
+          {...register("password2", { required: true })}
+          id="password2"
+          name="password2"
+          type="password2"
+        />
+        {errors.message && (
+          <span className="text-sm huge:text-base text-red-800 block text-right">
+            Campo obligatorio
+          </span>
+        )}
+        <br></br>
+
+        <button
+          className="transition hover:bg-hover block mx-auto mt-4 bg-lightblue text-white py-2 px-10 text-xs xl:text-base rounded-xl cursor-pointer"
+          type="submit"
+        >
+          Registrarme
+        </button>
+        <Link to="/" className="mx-auto text-lightblue hover:text-light">
+          Iniciar Sesión
+        </Link>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
