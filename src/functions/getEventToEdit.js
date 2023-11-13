08d69b492bb92643a3b4e4b09a0c7cd95c62b168
Milagros Gonzalez <101905632+MilagrosGonzalezS@users.noreply.github.com>
2023-11-13 12:37:51 -0300

    falto un save

--- src/functions/getEventToEdit.js
+++ src/functions/getEventToEdit.js
@@ -20,9 +20,9 @@ import axios from "axios";
 import config from "../config.json";
 
 //TRAER TODOS LOS EVENTOS
-async function getEventToEdit(eventId) {
+async function getEventById(eventId) {
   const res = await axios.get(`${config.apiEvents}/${eventId}`);
   return res;
 }
 
-export default getEventToEdit;
+export default getEventById;
